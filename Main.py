edeeb1907abe38ce1e473dd5fc790fa70d8ba4d7
AleksandraWozniak <olwen@interia.pl>
2017-10-31 14:38:02 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,18 +1,15 @@
-import sys
-if sys.version_info[0] < 3:
-    import got
-else:
-    import got3 as got
+#!/usr/bin/env python2
+import got
 
 def main():
 
 	def printTweet(descr, t):
-		print(descr)
-		print("Username: %s" % t.username)
-		print("Retweets: %d" % t.retweets)
-		print("Text: %s" % t.text)
-		print("Mentions: %s" % t.mentions)
-		print("Hashtags: %s\n" % t.hashtags)
+		print descr 
+		print "Username: %s" % t.username
+		print "Retweets: %d" % t.retweets
+		print "Text: %s" % t.text
+		print "Mentions: %s" % t.mentions
+		print "Hashtags: %s\n" % t.hashtags
 
 	# Example 1 - Get tweets by username
 	tweetCriteria = got.manager.TweetCriteria().setUsername('barackobama').setMaxTweets(1)
